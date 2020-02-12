a384240c4d23d013d27eabd61db51b10cbddbdd4
norio c <norionorioc@gmail.com>
2020-02-12 20:58:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
   array.join(" and ")
   
   elsif array.length == 3
-  lastStr = array.pop
+  array.insert 
   array.join(", ") << ", and #{lastStr}"
   
   else array.length > 3
