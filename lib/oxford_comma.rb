03e5489a64a6fbd2b16c9e4eb69b1b15cfdac09a
norio c <norionorioc@gmail.com>
2020-02-12 21:06:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  
+  if array.length == 1
+  array.join
+  
+  elsif array.length == 2
+  array.join(" and ")
+  
+  elsif array.length == 3
+  lastStr = array.pop
+  array.join(", ") << ", and #{lastStr}"
+  
+  else array.length > 3
+  lastStr = array.pop
+  array.join(", ") << ", and #{lastStr}"
+  
+  
+end
 end
